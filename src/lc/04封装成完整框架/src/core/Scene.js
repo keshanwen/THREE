f7350c7940afbc0100bb7eb39935f979b563c743
keshanwen <3640@winnermedical.com>
2024-10-16 10:19:57 +0800

    feat: Object3D Perspectivecamera Scene

--- src/lc/04封装成完整框架/src/core/Scene.js
+++ src/lc/04封装成完整框架/src/core/Scene.js
@@ -1,1 +1,9 @@
-export default {}
+import Object3D from "./Object3D.js";
+
+export default class Scene extends Object3D {
+  constructor() {
+    super();
+    this.type = "Scene";
+    this.background = [1, 1, 1, 1];
+  }
+}
